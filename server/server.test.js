bea92f3af1de2cc9b1559b57e384401118f89cc6
MasterCode20 <juniorngoumou724@gmail.com>
2024-07-09 18:15:59 +0200

    fix backend test

--- server/server.test.js
+++ server/server.test.js
@@ -2,10 +2,10 @@ const request = require("supertest");
 const express = require("express");
 
 const app = express();
-app.get("/ping", (req, res) => res.json({ msg: "Ping Successful" }));
+app.get("/ping", (req, res) => res.json({ msg: "Ping Successfully" }));
 
 describe("GET /ping", () => {
-    
+
   it("should return Ping Successful", async () => {
     const res = await request(app).get("/ping");
     expect(res.statusCode).toEqual(200);
