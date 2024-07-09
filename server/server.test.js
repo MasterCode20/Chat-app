3b8222408a51a9e278b244e5a76cc3309c5dd9b5
MasterCode20 <juniorngoumou724@gmail.com>
2024-07-09 17:59:27 +0200

    Add basic tests and update CI/CD pipeline

--- server/server.test.js
+++ server/server.test.js
@@ -5,6 +5,7 @@ const app = express();
 app.get("/ping", (req, res) => res.json({ msg: "Ping Successful" }));
 
 describe("GET /ping", () => {
+    
   it("should return Ping Successful", async () => {
     const res = await request(app).get("/ping");
     expect(res.statusCode).toEqual(200);
